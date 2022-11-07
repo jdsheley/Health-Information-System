fc8e73707bf9303654f6eb1fc0fda56ceccfa68d
jdsheley <70231392+jdsheley@users.noreply.github.com>
2022-11-06 19:56:42 -0500

    Update LoginController.java

---
+++ PatientInformationManagment/src/PatientManagment/LoginController.java
@@ -0,0 +1,29 @@
+package PatientManagment;
+
+public class LoginController {
+    private LoginUI theLoginUI;
+    private UserList theUserList;
+    public LoginCtrl(){
+        theUserList = new UserList();
+        theLoginUI = new LoginUI(this);
+
+        showLoginUI();
+    }
+    public UserList getUserList(){
+
+        return theUserList;
+    }
+    public void showLoginUI(){
+
+        theLoginUI.setVisible(true);
+    }
+    public void requestAuthenticate(String userName, String password){
+        if(theUserList.authenticate(userName, password)==true){
+            System.out.println("Valid user");
+        }
+        else
+            System.out.println("Invalid username/password");
+
+    }
+    
+}
