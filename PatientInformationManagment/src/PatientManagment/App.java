8a682200b2c504975de5ed194176a3b2b1290fea
Austin Besong <ajb7227@psu.edu>
2022-11-09 23:14:30 -0500

    added README2
    for individual design pattern submission

--- PatientInformationManagment/src/PatientManagment/App.java
+++ PatientInformationManagment/src/PatientManagment/App.java
@@ -10,14 +10,16 @@ import LoginPackage.*;
 public class App {
      
     public static void main(String[] args) throws Exception {
+        // panel for viewing the list of patients, will appear after logging in
         PatientTableController p = new PatientTableController();
 
+        // login panel, will come firs†
         LoginUI testLogin;
         LoginController loginController;
         loginController = new LoginController();
         testLogin = new LoginUI(loginController);
         loginController.showLoginUI(testLogin);
 
-        
+
     }
 }
