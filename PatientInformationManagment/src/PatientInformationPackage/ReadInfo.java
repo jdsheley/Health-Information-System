5e1e6eb0bb5136cb2728dbc1f711777341a66ef6
ethankonicek <eck5231@psu.edu>
2022-11-17 14:00:49 -0500

    Update

---
+++ PatientInformationManagment/src/PatientInformationPackage/ReadInfo.java
@@ -0,0 +1,20 @@
+package PatientInformationPackage;
+import java.io.File; 
+import java.io.IOException;  
+import LocationPackage.*;
+import PatientManagment.*;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class ReadInfo {
+
+    private File infoFile;
+
+    public ReadInfo(File infoFile) {
+        this.infoFile = infoFile;
+    }
+    
+}
