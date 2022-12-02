7ce9cd798f8fadfa76f7f97de6139c46e6a3aefd
jdsheley <70231392+jdsheley@users.noreply.github.com>
2022-12-02 17:53:11 -0500

    PatientTableUI fix
    fix to names not fully displaying

--- PatientInformationManagment/src/LoginPackage/PatientTableUI.java
+++ PatientInformationManagment/src/LoginPackage/PatientTableUI.java
@@ -10,9 +10,11 @@ public class PatientTableUI extends JFrame {
     private JTable patientTable;
     private JButton exitButton, selectButton, newPatientButton;
     private PatientTableController patientTableCntl;
+    private JScrollPane tableScroller;
 
     public PatientTableUI(PatientTableController patientTableCntl) {
         this.patientTableCntl = patientTableCntl;
+        initialTableComponents();
     }
 
     public void initialTableComponents() {
@@ -22,7 +24,8 @@ public class PatientTableUI extends JFrame {
         tablePanel = new JPanel();
         buttonPanel = new JPanel(new GridLayout(1, 4));
         patientTable = new JTable(patientTableCntl.getThePatientTableModel());
-        patientTable.getColumnModel().getColumn(0).setPreferredWidth(35);
+        patientTable.getColumnModel().getColumn(0).setPreferredWidth(25);
+        patientTable.getColumnModel().getColumn(1).setPreferredWidth(50);
         exitButton = new JButton("Exit");
         selectButton = new JButton("Select");
         newPatientButton = new JButton("New");
@@ -30,10 +33,15 @@ public class PatientTableUI extends JFrame {
         buttonPanel.add(selectButton);
         buttonPanel.add(newPatientButton);
         tablePanel.add(patientTable);
+        tableScroller = new JScrollPane(patientTable);
+        patientTable.setFillsViewportHeight(true);
+        tableScroller.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        tableScroller.setPreferredSize(new Dimension(700,300));
+        tablePanel.add(tableScroller);
         exitButton.addActionListener(new ExitButtonListener());
         selectButton.addActionListener(new SelectButtonListener());
         newPatientButton.addActionListener(new NewPatientButtonListener());
-        this.setSize(700, 500);
+        this.setSize(800, 400);
         this.setLocationRelativeTo(null);
         this.setContentPane(new JPanel(new BorderLayout()));
         this.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
@@ -58,6 +66,13 @@ public class PatientTableUI extends JFrame {
 
         @Override
         public void actionPerformed(ActionEvent e) {
+            Object obj = e.getSource();
+
+            int selectedTableRow = patientTable.getSelectedRow();
+            int selectedModelRow = patientTable.convertRowIndexToModel(selectedTableRow);
+            if (selectedModelRow < 0)
+                selectedModelRow = 0;
+            PatientTableUI.this.patientTableCntl.getPatientDetailsUI(selectedModelRow);
 
         }
     }
