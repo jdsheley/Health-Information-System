64f2062f0489342b9cda56e9e9d61ed4d5fbd513
Austin Besong <ajb7227@psu.edu>
2022-12-04 00:57:19 -0500

    PatientDetailsController created

---
+++ PatientInformationManagment/src/LoginPackage/PatientDetailsController.java
@@ -0,0 +1,43 @@
+package LoginPackage;
+import PatientInformationPackage.*;
+import PatientManagment.*;
+
+public class PatientDetailsController {
+    private PatientDetailsUI thePatientDetailsUI;
+    private UserList theUserList;
+    private PatientTableController thePatientTableController;
+
+    public PatientDetailsController(PatientTableController thePatientTableController, int row) {
+        this.thePatientTableController = thePatientTableController;
+
+        showPatientDetailsUI();
+    }
+
+    public PatientTableController getThePatientTableController() {
+        return thePatientTableController;
+    }
+
+    public void setThePatientTableController(PatientTableController thePatientTableController) {
+        this.thePatientTableController = thePatientTableController;
+    }
+
+    public PatientDetailsUI getThePatientDetailsUI() {
+        return thePatientDetailsUI;
+    }
+
+    public void setThePatientDetailsUI(PatientDetailsUI thePatientDetailsUI) {
+        this.thePatientDetailsUI = thePatientDetailsUI;
+    }
+
+    public UserList getTheUserList() {
+        return theUserList;
+    }
+
+    public void setTheUserList(UserList theUserList) {
+        this.theUserList = theUserList;
+    }
+
+    public void showPatientDetailsUI() {
+        thePatientDetailsUI.setVisible(true);
+    }
+}
