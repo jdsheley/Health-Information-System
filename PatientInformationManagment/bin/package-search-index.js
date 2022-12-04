c0778780fb05424ba36451d9ac57cec92e61f36a
ethankonicek <eck5231@psu.edu>
2022-12-03 22:50:48 -0500

    fuck

--- PatientInformationManagment/bin/package-search-index.js
+++ PatientInformationManagment/bin/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"PatientManagment"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"LocationPackage"}]
